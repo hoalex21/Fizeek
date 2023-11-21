c5ad3972e3b78ebd62314b995648d7b1612bb69e
Alex Ho <alexho@Alexs-Air.fios-router.home>
2023-11-20 20:35:33 -0500

    create navbar with links to homepage and signup, add signup route

---
+++ app/ui/navbar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+
+export default function NavBar() {
+    return (
+        <nav className="max-w-screen flex justify-center shadow-inner border-b-2 p-4">
+            <div className="container flex justify-between">
+                <div>
+                    <Link href="/">Fizeek.</Link>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <Link href="/signup">Sign Up</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+       </nav>
+    );
+}
