1121702680123834bfe51574015d2d1ae4fa0a1d
Alex Ho <alexho@Alexs-Air.fios-router.home>
2023-11-20 22:59:54 -0500

    create signup form frontend only

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,45 @@
+import Link from "next/link";
+import UserInput from "../ui/user-input";
+
 export default function SignUp() {
-    return <></>;
+    return (
+        <div className="flex justify-center items-center mt-[calc(59px)]">
+            <div className="border-2 shadow rounded-md p-5 lg:w-5/12 xl:w-4/12">
+                <form>
+                    <fieldset>
+                        <legend className="text-center">
+                            Create a Fizeek account
+                        </legend>
+
+                        <hr />
+                        <br />
+                        
+                        <UserInput label="First Name" type="text" id="first-name" name="first-name"/>
+                        <br/> <br/>
+                        <UserInput label="Last Name" type="text" id="last-name" name="last-name"/>
+                        <br/> <br/>
+                        <UserInput label="Email" type="text" id="email" name="email"/>
+                        <br/> <br/>
+                        <UserInput label="Email Confirmation" type="text" id="email-confirmation" name="email-confirmation"/>
+                        <br/> <br/>
+                        <UserInput label="Username" type="text" id="username" name="username"/>
+                        <br/> <br/>
+                        <UserInput label="Password" type="password" id="password" name="password"/>
+                        <br/> <br/>
+                        <UserInput label="Password Confirmation" type="password" id="password-confirmation" name="password-confirmation"/>
+                    </fieldset>
+
+                    <br />
+
+                    <div className="flex justify-between items-center">
+                        <p>Already have an account? Log in <Link href="/login" className="text-blue-500">here</Link>.</p>
+                        
+                        <button type="submit" className="bg-black text-white rounded-lg p-2">
+                            Sign Up
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
