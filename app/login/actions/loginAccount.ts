09a4a971ba9aff54960e58145c83edb6e9927bad
Alex Ho <alexho@Alexs-Air.fios-router.home>
2023-11-25 23:16:41 -0500

    make LoginAccount a server component, add password validation

--- app/login/actions/loginAccount.ts
+++ app/login/actions/loginAccount.ts
@@ -1,11 +1,24 @@
+"use server";
+
 import { redirect } from "next/navigation";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default async function LoginAccount(prevState: any, formData: FormData) {
     const email = formData.get("email")?.toString();
     const password = formData.get("password")?.toString();
 
     if (email && password) {
-        redirect("/");
+        const user = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        });
+
+        if (user?.password === password) {
+            redirect("/");
+        }
     }
 
     return prevState;
