dedba6bfe67af0265e82d5c304a1fc5ed70db9c0
Alex Ho <alexho@n3-91-203.dhcp.drexel.edu>
2023-11-28 15:11:47 -0500

    add signout client component

--- app/signout/page.tsx
+++ app/signout/page.tsx
@@ -3,13 +3,12 @@
 import { signOut, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 
-export default async function SignOut() {
+export default function SignOut() {
     const { data: session, status } = useSession();
 
-    if (status === "authenticated") {
-        // FIXME: causing errors in console
-        await signOut({callbackUrl: "/"});
+    if (status === "unauthenticated") {
+        redirect("/signup");
+    } else {
+        signOut({ redirect: false, callbackUrl: "/" });
     }
-    
-    redirect("/login");
 }
