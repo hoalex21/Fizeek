70151d055366e35e41b12b493fa67051ae023fb6
Alex Ho <alexho@Alexs-Air.fios-router.home>
2023-11-29 11:29:02 -0500

    add image and test to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
-import NavBar from './ui/navbar'
+import Image from 'next/image';
+import Link from 'next/link';
+import NavBar from './ui/navbar';
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div style={{
+        zIndex: -1,
+        position: 'absolute',
+        width: "100vw",
+        height: "calc(100vh - 56px)",
+      }}>
+        <Image
+          src="/deadlift.jpg" 
+          alt="A man deadlifting"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+
+      <div className='flex h-[calc(100vh-56px)] text-center'>
+        <div className='m-auto text-5xl text-white'>
+          <p>Achieve your physique goals with Fizeek.</p>
+          <br />
+          <p><Link href="/signup" className='text-blue-500'>Sign up</Link> for an account today.</p>
+        </div>
+      </div>
+    </>
+  );
 }
