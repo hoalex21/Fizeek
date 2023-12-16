6381fcc3b7836f3e2380b8908f355d9f69b2c8c0
Alex Ho <alexho@n3-119-94.dhcp.drexel.edu>
2023-12-15 19:13:22 -0500

    add initial signup test with cypress

---
+++ cypress/e2e/User.cy.ts
@@ -0,0 +1,27 @@
+const firstName = "Big";
+const lastName = "Baby"
+const email = "bb@gmail.com";
+const username = "bby";
+const password = "B!1bbbbb";
+
+
+describe('signup', async () => {
+  it('New user should be able to signup for an account', async () => {
+    // Arrange
+    cy.exec('npx prisma migrate reset --force');
+    cy.visit("/auth/signup");
+
+    // Act
+    cy.get('input[id="first-name"]').type(firstName);
+    cy.get('input[id="last-name"]').type(lastName);
+    cy.get('input[id="email"]').type(email);
+    cy.get('input[id="email-confirmation"]').type(email);
+    cy.get('input[id="username"]').type(username);
+    cy.get('input[id="password"]').type(password);
+    cy.get('input[id="password-confirmation"]').type(password);
+    cy.get('button[type="submit"]').click();
+
+    // Assert
+    cy.url().should('include', 'auth/login');
+  });
+});
