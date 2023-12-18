be292e6210aaa12aba921da712fcc525069749be
Alex Ho <alexho@Alexs-Air.fios-router.home>
2023-12-18 01:13:19 -0500

    update user height and weight

---
+++ app/settings/anthropometry/actions/anthropometryAction.ts
@@ -0,0 +1,77 @@
+"use server";
+
+import prisma from "@/db";
+
+export default async function AnthropometryAction(email: string, prevState: any, formData: FormData) {
+    const user = await prisma.user.findMany({
+        where: {
+            email: email
+        }
+    });
+
+    const centimeters = formData.get("centimeters")?.toString();
+    const kilograms = formData.get("kilograms")?.toString();
+
+    if (user[0] && centimeters && kilograms) {
+        await prisma.user.update({
+            where: {
+                id: user[0].id
+            },
+            data: {
+                height: {
+                    upsert: {
+                        create: {
+                            centimeters: parseInt(centimeters)
+                        },
+                        update: {
+                            centimeters: parseInt(centimeters)
+                        }
+                    }
+                }
+            }
+        });
+
+        await prisma.user.update({
+            where: {
+                id: user[0].id
+            },
+            data: {
+                weight: {
+                    upsert: {
+                        create: {
+                            kilograms: parseInt(kilograms)
+                        },
+                        update: {
+                            kilograms: parseInt(kilograms)
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    const height = await prisma.height.findFirst({
+        where: {
+            userId: user[0]?.id
+        }
+    });
+
+    const weight = await prisma.weight.findFirst({
+        where: {
+            userId: user[0]?.id
+        }
+    });
+
+    console.log(email);
+    console.log(user);
+    console.log(height?.centimeters);
+    console.log(weight?.kilograms);
+}
+
+async function getUser(email: string) {
+    return await prisma.user.findFirst({
+        where: {
+            email: email
+        }
+    });
+}
--- app/settings/anthropometry/page.tsx
+++ app/settings/anthropometry/page.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useFormState } from "react-dom";
+import AnthropometryAction from "./actions/anthropometryAction";
+
+const initialState = {
+    data: null
+}
+
 export default function Anthropometry() {
-    return <p>Anthropometry</p>;
+    const { data: session, status } = useSession();
+    const AnthropometryActionWithEmail = AnthropometryAction.bind(null, session?.user?.email ? session.user.email : "");
+    const [state, formAction] = useFormState(AnthropometryActionWithEmail, initialState);
+
+    return (
+        <div>
+            <div className="flex justify-center">
+                <span className="text-2xl">Anthropometry</span>
+            </div>
+            <form action={formAction}>
+                <span>Height: </span>
+                <input className="w-20 border-2 rounded-md pl-1" id="centimeters" name="centimeters" />
+                <label> Centimeters</label>
+
+                <br></br>
+                <br></br>
+
+                <span>Weight: </span>
+                <input className="w-20 border-2 rounded-md pl-1" id="kilograms" name="kilograms" />
+                <label> Kilograms</label>
+
+                <br></br>
+                <br></br>
+
+                <button type="submit" className="bg-blue-500 text-white rounded-lg px-6 py-2">Save</button>
+            </form>
+        </div>
+    );
 }
