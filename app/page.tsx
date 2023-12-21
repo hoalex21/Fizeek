04b600411d0b85937da550da7284869b79876dda
Alex Ho <alexho@Alexs-Air.fios-router.home>
2023-12-21 12:28:03 -0500

    move background image outside of grid

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,15 @@ export default function Home() {
     <>
       <NavBar />
 
+      <div className='-z-10 absolute w-screen h-[calc(100vh-56px)]'>
+        <Image
+          src="/back.jpg" 
+          alt="A bodybuilder performing a back pose."
+          fill={true}
+          style={{objectFit: "cover"}}
+        />
+      </div>
+
       <main>
         <div className='h-[calc(100vh-56px)] grid grid-cols-2'>
           <div className='text-white flex justify-center md:col-span-1 col-span-2 gap-1 items-center'>
@@ -14,14 +23,6 @@ export default function Home() {
               <p className='text-6xl'>Achieve your best physique with Fizeek</p> <br></br>
               <p>Solutions to help you reach your health and fitness goals</p> <br></br>
               <Link className="bg-black p-1 border-2 border-white text-white px-4 py-2" href="/auth/signup">Sign Up</Link>
-            </div>
-            <div className='-z-10'>
-              <Image
-                src="/back.jpg" 
-                alt="A bodybuilder performing a back pose."
-                fill={true}
-                style={{objectFit: "cover"}}
-              />
             </div>
           </div>
         </div>
