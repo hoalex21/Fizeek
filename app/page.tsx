e3eab8d8e1cba581b4ea88ae5d5c8f411d0ba697
Alex Ho <alexho@Alexs-Air.fios-router.home>
2023-12-21 15:55:57 -0500

    show sign up button if unauthenticated

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import Image from 'next/image';
 import Link from 'next/link';
 import NavBar from './ui/navbar';
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
+  const { data: session, status } = useSession();
+
   return (
     <>
       <NavBar />
@@ -18,11 +23,15 @@ export default function Home() {
 
       <main className='text-white'>
         <div className='h-[calc(100vh-56px)] grid grid-cols-2'>
-          <div className='flex items-center md:col-span-1 col-span-2 gap-1'>
+          <div className='flex items-center lg:col-span-1 col-span-2 gap-1'>
             <div className='px-10'>
               <p className='text-6xl'>Achieve your best physique with Fizeek</p> <br></br>
               <p>Solutions to help you reach your health and fitness goals</p> <br></br>
-              <Link className="bg-black p-1 border-2 border-white text-white px-4 py-2" href="/auth/signup">Sign Up</Link>
+              {
+                status === "unauthenticated" ? 
+                <Link className="bg-black p-1 border-2 border-white text-white px-4 py-2" href="/auth/signup">Sign Up</Link> : 
+                <div hidden></div>
+              }
             </div>
           </div>
         </div>
@@ -42,7 +51,7 @@ export default function Home() {
 
         <div className='h-[50vh]'>
           <div className='h-full grid grid-cols-2'>
-            <div className='flex items-center md:col-span-1 col-span-2 gap-1'>
+            <div className='flex items-center lg:col-span-1 col-span-2 gap-1'>
               <div className='px-20'>
                 <p className='text-5xl'>Calculators for measuring your biometrics</p> <br></br>
                 <Link className="bg-black p-1 border-2 border-white text-white px-4 py-2 mr-2" href="/calculator/bmi">BMI</Link>
