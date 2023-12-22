377b57d792a5cd7064b84e9a6abc6219a048fcc9
Alex Ho <alexho@Alexs-Air.fios-router.home>
2023-12-21 20:00:08 -0500

    unround navbar buttons, change login to log in

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -11,7 +11,7 @@ export default function NavBar() {
         <nav className="max-w-screen shadow p-4 flex justify-center bg-white">
             <div className="container grid grid-cols-3 content-center">
                 <div>
-                    <Link href="/" className="font-bold">Fizeek.</Link>
+                    <Link href="/" className="font-bold">Fizeek</Link>
                 </div>
 
                 {/* Site Features */}
@@ -42,10 +42,10 @@ export default function NavBar() {
                             ) : (
                                 <>
                                     <li className="mr-2">
-                                        <Link className="rounded-lg bg-black p-1 border-2 border-black text-white px-4 py-2" href="/auth/signup">Sign Up</Link>
+                                        <Link className="bg-black p-1 border-2 border-black text-white px-4 py-2" href="/auth/signup">Sign Up</Link>
                                     </li>
                                     <li>
-                                        <Link className="rounded-lg p-1 bg-white border-2 border-black px-4 py-2" href="/auth/login">Login</Link>
+                                        <Link className="p-1 bg-white border-2 border-black px-4 py-2" href="/auth/login">Log In</Link>
                                     </li>
                                 </>
                             )
